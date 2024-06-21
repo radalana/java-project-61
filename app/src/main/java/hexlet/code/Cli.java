9beebfc81fbb3b4d5ed4d1054c34851b1ab24d22
Svetlana <radalana415@gmail.com>
2024-06-21 16:48:31 +0200

    Add function greetUser()

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void greetUser() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I ask your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName+ "!");
+        scanner.close();
+    }
+}
