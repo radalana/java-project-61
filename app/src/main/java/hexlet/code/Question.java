598ea7dcc61758d40bbb02f35314b962c8e8df1b
Svetlana <radalana415@gmail.com>
2024-06-26 17:01:07 +0200

    Question.java
    hiddenField fix

--- app/src/main/java/hexlet/code/Question.java
+++ app/src/main/java/hexlet/code/Question.java
@@ -8,9 +8,9 @@ public class Question {
     private final String question;
     private final String answer;
 
-    public Question(String question, String answer) {
-        this.question = question;
-        this.answer = answer;
+    public Question(String task, String correctAnswer) {
+        this.question = task;
+        this.answer = correctAnswer;
     }
 
     /**
