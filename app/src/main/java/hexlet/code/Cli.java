c7873a85c728670ec39ddd9b5cf3e50e0a819d6b
Svetlana <radalana415@gmail.com>
2024-06-26 16:18:36 +0200

    Refactor Cli class

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -12,7 +12,7 @@ public class Cli {
     public static String getUserName() {
 
         System.out.println("Welcome to the Brain Games!");
-        System.out.print("May I have your name? ");
+        System.out.print("May I have your name?");
         String userName = scanner.next();
         System.out.println("Hello, " + userName + "!");
 
