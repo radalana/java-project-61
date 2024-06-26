76c1ce4c06d93973d05a2b2de72371e00bdd518a
Svetlana <radalana415@gmail.com>
2024-06-26 16:44:32 +0200

    Refactor class Question
    add comments

--- app/src/main/java/hexlet/code/Question.java
+++ app/src/main/java/hexlet/code/Question.java
@@ -1,19 +1,33 @@
 package hexlet.code;
 
+/**
+ * This class represents a question with an answer.
+ * It can be extended to add more functionalities.
+ */
 public class Question {
-    private String question;
-    private String answer;
+    private final String question;
+    private final String answer;
 
     public Question(String question, String answer) {
         this.question = question;
         this.answer = answer;
     }
 
+    /**
+     * Returns the question.
+     *
+     * @return the question
+     */
     public String getQuestion() {
-        return question;
+        return this.question;
     }
 
+    /**
+     * Returns the answer to the question.
+     *
+     * @return The answer as a string.
+     */
     public String getAnswer() {
-        return answer;
+        return this.answer;
     }
 }
