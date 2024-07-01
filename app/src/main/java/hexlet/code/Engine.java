6f6141546d51a1a24825399cd846773d5bc8cdbb
Svetlana <radalana415@gmail.com>
2024-07-01 14:30:05 +0200

    Refactor Engine.java
    * Delete functction getName
    * Consololidate to flow()

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -5,16 +5,12 @@ import java.util.Scanner;
 public class Engine {
     public static final int ROUNDS = 3;
     public static final Scanner SCANNER = new Scanner(System.in);
-    public static String getUserName() {
+
+    public static void flow(String description, Question[] questions) {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name?");
         String userName = SCANNER.next();
         System.out.println("Hello, " + userName + "!");
-        return userName;
-    }
-
-    public static void flow(String description, Question[] questions) {
-        String userName = getUserName();
         System.out.println(description);
         for (int i = 0; i < ROUNDS; i++) {
             System.out.println("Question: " + questions[i].getQuestion());
